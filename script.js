ae52903b67f54b1b240e724908ee223601f701e9
DaniloSanna0 <102580223+DaniloSanna0@users.noreply.github.com>
2022-12-21 15:33:18 +0100

    sessionStorage

---
+++ script.js
@@ -0,0 +1,19 @@
+function success() {
+    if(document.getElementById("validationTextarea").value.length != 10 || document.getElementById("invalidCheck").checked == false) { 
+           document.getElementById('btn').disabled = true; 
+       } else { 
+           document.getElementById('btn').disabled = false;
+       }
+       console.log(document.getElementById("invalidCheck").checked );
+       sessionStorage.setItem("numero",document.getElementById("validationTextarea").value)
+   }
+
+
+   const form = document.getElementById("btn");
+   form.addEventListener("click", function(event){
+    event.preventDefault()
+
+    let numero = sessionStorage.getItem("numero");
+    document.getElementById("numero").innerHTML = numero;
+    // document.getElementById("validationTextarea").value = document.getElementById("numero").innerHTML
+  });
